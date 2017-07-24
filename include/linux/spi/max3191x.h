d0617540dfad2fafa7daa27269a0d9ce610c7aa7
Lukas Wunner <lukas@wunner.de>
2017-07-24 17:01:22 +0200

    gpio: max3191x: Allow in-kernel configuration of modesel pin
    Allow switching back and forth between 16-bit and 8-bit mode,
    prioritizing either safety or speed.
    
    Cc: Mathias Duckeck <m.duckeck@kunbus.de>
    Signed-off-by: Lukas Wunner <lukas@wunner.de>

---
+++ include/linux/spi/max3191x.h
@@ -0,0 +1,23 @@
+/*
+ * max3191x.h - in-kernel API for Maxim MAX3191x industrial serializer
+ *
+ * Copyright (C) 2017 KUNBUS GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2) as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _MAX3191X_H_
+#define _MAX3191X_H_
+
+#include <linux/device.h>
+
+enum max3191x_mode {
+	STATUS_BYTE_ENABLED,
+	STATUS_BYTE_DISABLED,
+};
+
+int max3191x_set_mode(struct device *dev, enum max3191x_mode mode);
+
+#endif
