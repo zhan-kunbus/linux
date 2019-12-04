ddfb39bcbb65ed4ae36c53d1e28724641aed60a4
Lukas Wunner <lukas@wunner.de>
2019-12-04 12:16:28 +0100

    serial: 8250: Support rs485 bus termination GPIO
    Commit e8759ad17d41 ("serial: uapi: Add support for bus termination")
    introduced the ability to enable rs485 bus termination from user space.
    So far the feature is only used by a single driver, 8250_exar.c, using a
    hardcoded GPIO pin specific to Siemens IOT2040 products.
    
    Provide for a more generic solution by allowing specification of an
    rs485 bus termination GPIO pin in the device tree:  Amend the serial
    core to retrieve the GPIO from the device tree (or ACPI table) and amend
    the default ->rs485_config() callback for 8250 drivers to change the
    GPIO on request from user space.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>

--- drivers/tty/serial/8250/8250_port.c
+++ drivers/tty/serial/8250/8250_port.c
@@ -685,6 +685,10 @@ int serial8250_em485_config(struct uart_port *port, struct serial_rs485 *rs485)
 	memset(rs485->padding, 0, sizeof(rs485->padding));
 	port->rs485 = *rs485;
 
+	if (port->rs485_term_gpio)
+		gpiod_set_value(port->rs485_term_gpio,
+				rs485->flags & SER_RS485_TERMINATE_BUS);
+
 	/*
 	 * Both serial8250_em485_init() and serial8250_em485_destroy()
 	 * are idempotent.
--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -22,6 +22,7 @@
 
 #include <linux/bitops.h>
 #include <linux/compiler.h>
+#include <linux/gpio/consumer.h>
 #include <linux/interrupt.h>
 #include <linux/circ_buf.h>
 #include <linux/spinlock.h>
@@ -261,6 +262,7 @@ struct uart_port {
 	struct attribute_group	*attr_group;		/* port specific attributes */
 	const struct attribute_group **tty_groups;	/* all attributes (serial core use only) */
 	struct serial_rs485     rs485;
+	struct gpio_desc	*rs485_term_gpio;	/* enable RS485 bus termination */
 	void			*private_data;		/* generic platform data pointer */
 };
 
