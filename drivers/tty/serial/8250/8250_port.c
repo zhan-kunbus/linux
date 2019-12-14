668a4f88c8cde3c2e503a13cc473e7e53fe6e0e0
Lukas Wunner <lukas@wunner.de>
2019-12-14 09:57:34 +0100

    serial: 8250: Deduplicate rs485 active_timer assignment
    When rs485 transmission over an 8250 port stops, __stop_tx() assigns
    active_timer = NULL before calling __stop_tx_rs485().
    
    That function in turn either assigns active_timer = stop_tx_timer and
    rearms the timer (in case a delay_rts_after_send needs to be observed)
    or directly calls __do_stop_tx_rs485().
    
    Move the assignment active_timer = NULL to __stop_tx_rs485() into the
    branch which directly calls __do_stop_tx_rs485(), thereby avoiding a
    duplicate assignment and simplifying the code.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Cc: Matwey V. Kornilov <matwey@sai.msu.ru>

--- drivers/tty/serial/8250/8250_port.c
+++ drivers/tty/serial/8250/8250_port.c
@@ -1543,6 +1543,7 @@ static void __stop_tx_rs485(struct uart_8250_port *p)
 				   p->port.rs485.delay_rts_after_send);
 	} else {
 		__do_stop_tx_rs485(p);
+		em485->active_timer = NULL;
 	}
 }
 
@@ -1569,8 +1570,6 @@ static inline void __stop_tx(struct uart_8250_port *p)
 		 */
 		if ((lsr & BOTH_EMPTY) != BOTH_EMPTY)
 			return;
-
-		em485->active_timer = NULL;
 
 		__stop_tx_rs485(p);
 	}
