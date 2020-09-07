c2e72eedd6d99a22a557ca183e761b01eaf5c309
Philipp Rosenberger <p.rosenberger@kunbus.com>
2020-09-07 13:24:02 +0200

    smsc95xx: fix indention of smsc95xx_is_macaddr_param()
    The smsc95xx_is_macaddr_param() is wrong indented. This was introduced
    with commit aac7b105788e ("Allow mac address to be set in smsc95xx").
    
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- drivers/net/usb/smsc95xx.c
+++ drivers/net/usb/smsc95xx.c
@@ -929,48 +929,48 @@ static int smsc95xx_ioctl(struct net_device *netdev, struct ifreq *rq, int cmd)
 /* Check the macaddr module parameter for a MAC address */
 static int smsc95xx_is_macaddr_param(struct usbnet *dev, u8 *dev_mac)
 {
-       int i, j, got_num, num;
-       u8 mtbl[MAC_ADDR_LEN];
+	int i, j, got_num, num;
+	u8 mtbl[MAC_ADDR_LEN];
 
-       if (macaddr[0] == ':')
-               return 0;
+	if (macaddr[0] == ':')
+		return 0;
 
-       i = 0;
-       j = 0;
-       num = 0;
-       got_num = 0;
-       while (j < MAC_ADDR_LEN) {
-               if (macaddr[i] && macaddr[i] != ':') {
-                       got_num++;
-                       if ('0' <= macaddr[i] && macaddr[i] <= '9')
-                               num = num * 16 + macaddr[i] - '0';
-                       else if ('A' <= macaddr[i] && macaddr[i] <= 'F')
-                               num = num * 16 + 10 + macaddr[i] - 'A';
-                       else if ('a' <= macaddr[i] && macaddr[i] <= 'f')
-                               num = num * 16 + 10 + macaddr[i] - 'a';
-                       else
-                               break;
-                       i++;
-               } else if (got_num == 2) {
-                       mtbl[j++] = (u8) num;
-                       num = 0;
-                       got_num = 0;
-                       i++;
-               } else {
-                       break;
-               }
-       }
+	i = 0;
+	j = 0;
+	num = 0;
+	got_num = 0;
+	while (j < MAC_ADDR_LEN) {
+		if (macaddr[i] && macaddr[i] != ':') {
+			got_num++;
+			if ('0' <= macaddr[i] && macaddr[i] <= '9')
+				num = num * 16 + macaddr[i] - '0';
+			else if ('A' <= macaddr[i] && macaddr[i] <= 'F')
+				num = num * 16 + 10 + macaddr[i] - 'A';
+			else if ('a' <= macaddr[i] && macaddr[i] <= 'f')
+				num = num * 16 + 10 + macaddr[i] - 'a';
+			else
+				break;
+			i++;
+		} else if (got_num == 2) {
+			mtbl[j++] = (u8) num;
+			num = 0;
+			got_num = 0;
+			i++;
+		} else {
+			break;
+		}
+	}
 
-       if (j == MAC_ADDR_LEN) {
-               netif_dbg(dev, ifup, dev->net, "Overriding MAC address with: "
-               "%02x:%02x:%02x:%02x:%02x:%02x\n", mtbl[0], mtbl[1], mtbl[2],
-                                               mtbl[3], mtbl[4], mtbl[5]);
-               for (i = 0; i < MAC_ADDR_LEN; i++)
-                       dev_mac[i] = mtbl[i];
-               return 1;
-       } else {
-               return 0;
-       }
+	if (j == MAC_ADDR_LEN) {
+		netif_dbg(dev, ifup, dev->net, "Overriding MAC address with: "
+		"%02x:%02x:%02x:%02x:%02x:%02x\n", mtbl[0], mtbl[1], mtbl[2],
+						mtbl[3], mtbl[4], mtbl[5]);
+		for (i = 0; i < MAC_ADDR_LEN; i++)
+			dev_mac[i] = mtbl[i];
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 static void smsc95xx_init_mac_address(struct usbnet *dev)
